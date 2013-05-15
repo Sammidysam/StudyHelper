54a0be018c059f82f5caaf8814e80e9fef6caf88
Sammidysam <sammcraig@yahoo.com>
2013-05-15 19:10:26 -0400

    Added Mac compatibility
    Mac compatibility is untested however, so more testing will be necessary
    later.  Also, added a convenient launchApplication() method that will
    launch your application depending on the OS.  Also made constants
    WINDOWS LINUX and MAC.  On Windows gcc they are not used, so they should
    be safe.

--- src/main.c
+++ src/main.c
@@ -3,25 +3,53 @@
 #elif _WIN32
 #define OS 0
 #elif __APPLE__
-#define OS -1
+#define OS 2
 #elif __linux
 #define OS 1
 #elif __unix
 #define OS 1
 #elif __posix
 #define OS 1
+#else
+#define OS -1
 #endif
 
+#define WINDOWS 0
+#define LINUX 1
+#define MAC 2
+
 #include <stdio.h>
+#include <string.h>
+
+void launchApplication(char *appName){
+	char command [60];
+	switch (OS){
+		case WINDOWS:
+			strcpy(command, appName);
+			system(command);
+			break;
+		case LINUX:
+			strcpy(command, "./");
+			strcat(command, appName);
+			system(command);
+			break;
+		case MAC:
+			// untested code
+			strcpy(command, "open -n ./");
+			strcat(command, appName);
+			system(command);
+			break;
+		default:
+			printf("Illegal OS value!\n");
+			break;
+	}
+}
 
 int main(int argc, char *argv[]){
-	printf("Platform Value:  %d\n", OS);
-	if(OS == 1)
-		system("./LessonSpanish");
-	else if(OS == 0)
-		system("LessonSpanish.exe");
-	else
-		printf("Your platform is not supported!\n");
+	printf("-----INIT-------\n");
+	printf("Platform Value:  %i\n", OS);
+	printf("-----ENDINIT----\n");
+	launchApplication("LessonSpanish");
 }
 
 
