0b2e417a94d3e475f69542f434cd24f986c0a954
Sam Craig <sammidysam@gmail.com>
2013-05-16 21:10:40 -0400

    Search for directories StudyHelper can now search for directories.

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 bin=bin
 src=src
 obj=obj
+flags=-std=c11 -O3
+
 dirs:=$(sort $(dir $(call rwildcard,$(src)/*/,*)))
 
 prog=$(bin)/StudyHelper
@@ -37,17 +39,17 @@ binary: $(prog)
 	$(foreach x,$(dirs),$(call innerprogram,$(x)))
 	
 $(obj)/main.o: $(src)/main.c
-	gcc -c -o $(obj)/main.o $(src)/main.c -O3
+	gcc -c -o $(obj)/main.o $(src)/main.c $(flags)
 	
 $(prog): $(obj)/main.o
-	gcc -o $(prog) $(obj)/main.o -O3
+	gcc -o $(prog) $(obj)/main.o $(flags)
 	
 define innerprogram
 
 -rm $(obj)/*.o
 -del $(obj)\*.o
-gcc -c -o $(obj)/$(patsubst %.c,%.o,$(notdir $(call rwildcard,$1,*.c))) $(call rwildcard,$1,*.c) -O3
-gcc -o $(bin)/Lesson$(notdir $(patsubst %/,%,$1)) $(obj)/*.o -O3
+gcc -c -o $(obj)/$(patsubst %.c,%.o,$(notdir $(call rwildcard,$1,*.c))) $(call rwildcard,$1,*.c) $(flags)
+gcc -o $(bin)/Lesson$(notdir $(patsubst %/,%,$1)) $(obj)/*.o $(flags)
 	
 endef
 	
--- src/main.c
+++ src/main.c
@@ -19,7 +19,32 @@
 #define MAC 2
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <glob.h>
+
+const char *pattern = OS == WINDOWS ? ".\\Lesson*" : "./Lesson*";
+
+int getNumberOfExecutables(){
+	glob_t result;
+	glob(pattern, GLOB_ERR, NULL, &result);
+	int number = 0;
+	int i = 0;
+	for(i = 0; i < result.gl_pathc; i++)
+		if(result.gl_pathv[i] != NULL)
+			number++;
+	globfree(&result);
+	return number;
+}
+
+void getExecutables(char **list){
+	glob_t result;
+	glob(pattern, GLOB_ERR, NULL, &result);
+	int i = 0;
+	for(i = 0; i < getNumberOfExecutables(); i++)
+		strcpy(list[i], result.gl_pathv[i]);
+	globfree(&result);
+}
 
 void launchApplication(char *appName){
 	char command [60];
@@ -49,7 +74,16 @@ int main(int argc, char *argv[]){
 	printf("-----INIT-------\n");
 	printf("Platform Value:  %i\n", OS);
 	printf("-----ENDINIT----\n");
-	launchApplication("LessonSpanish");
+	char **list = (char**)malloc(getNumberOfExecutables() * sizeof(char*));
+	for(int i = 0; i < getNumberOfExecutables(); i++){
+		list[i] = (char*)malloc(60);
+	}
+	getExecutables(list);
+	for(int i = 0; i < getNumberOfExecutables(); i++){
+		printf("%s\n", list[i]);
+		launchApplication(list[i]);
+	}
+	return 0;
 }
 
 
