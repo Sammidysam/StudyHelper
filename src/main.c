c75aa0791d2b19968c8646fef8940105489b31f6
Sam Craig <sammidysam@gmail.com>
2013-05-16 21:13:40 -0400

    For loops For loops now use inital values, which is a lot better than using a whole line to make the loop variable.

--- src/main.c
+++ src/main.c
@@ -29,8 +29,7 @@ int getNumberOfExecutables(){
 	glob_t result;
 	glob(pattern, GLOB_ERR, NULL, &result);
 	int number = 0;
-	int i = 0;
-	for(i = 0; i < result.gl_pathc; i++)
+	for(int i = 0; i < result.gl_pathc; i++)
 		if(result.gl_pathv[i] != NULL)
 			number++;
 	globfree(&result);
@@ -40,8 +39,7 @@ int getNumberOfExecutables(){
 void getExecutables(char **list){
 	glob_t result;
 	glob(pattern, GLOB_ERR, NULL, &result);
-	int i = 0;
-	for(i = 0; i < getNumberOfExecutables(); i++)
+	for(int i = 0; i < getNumberOfExecutables(); i++)
 		strcpy(list[i], result.gl_pathv[i]);
 	globfree(&result);
 }
