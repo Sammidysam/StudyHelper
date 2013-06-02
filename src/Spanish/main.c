bdb85bef3017a2eaba2575431ad5d199f835bca1
Sammidysam <sammidysam@gmail.com>
2013-06-02 15:51:50 -0400

    Moved lessons Project folder was getting too cramped, so lessons were moved to "lessons" directory.

--- src/Spanish/main.c
+++ src/Spanish/main.c
@@ -6,26 +6,39 @@
 #include "files.h"
 #include "errorlog.h"
 #include "boolean.h"
+#include "../osdetect.h"
 
 char questionFile [260], answerFile [260];
 bool checkCorrect = false;
 //note: checkCorrect does not work.  It marks everything as incorrect.  Only turn to true when compiling if you are fixing it.
 
 int main(int argc, char *argv[]){
-	getLineLoop("Type the name of the questions file.\n", questionFile, sizeof(questionFile));
+	printf("All lessons should be in the lessons directory.\n");
+	printf("When prompted, just type the name of the lesson.  Do not type the file extension or the directory it is in.\n");
+	printf("For example, if you had the lesson with question file 4BQ.txt and answer file 4BA.txt in the lessons folder, you would simply type \"4BQ.txt\" for the question file and \"4BA.txt\" for the answer file.\n");
+	if(OS == WINDOWS){
+		strcpy(questionFile, "lessons\\");
+		strcpy(answerFile, "lessons\\");
+	} else {
+		strcpy(questionFile, "lessons/");
+		strcpy(answerFile, "lessons/");
+	}
+	char name [260];
+	getLineLoop("Type the name of the questions file.\n", name, sizeof(name));
+	strcat(questionFile, name);
 	strcat(questionFile, ".txt");
-	getLineLoop("Type the name of the answers file.\n", answerFile, sizeof(answerFile));
+	getLineLoop("Type the name of the answers file.\n", name, sizeof(name));
+	strcat(answerFile, name);
 	strcat(answerFile, ".txt");
 	srand(time(NULL));
 	const int lines = getLines(questionFile, answerFile);
 	if(lines < 0)
 		return lines;
 	char question [256], answer [256], input [256];
-	int questionLine;
 	bool done = false;
 	printf("Type 'stop' to stop.\n");
 	while(!done){
-		questionLine = rand() % lines;
+		int questionLine = rand() % lines;
 		getFileLine(question, questionLine, questionFile);
 		printf("%s", question);
 		getLineLoop("", input, sizeof(input));
